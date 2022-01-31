324420d284b474f7001ab3eb5a1992e8752a1d42
Michael Hajný <47132583+3n0wd3n@users.noreply.github.com>
2022-01-31 18:07:33 +0100

    Update script.js

---
+++ js/script.js
@@ -0,0 +1,36 @@
+//select DOM
+outputInSeconds = document.querySelector('.seconds');
+outputInMinutes = document.querySelector('.minutes');
+outputInHours = document.querySelector('.hours');
+outputInMonths = document.querySelector('.months');
+outputInYears = document.querySelector('.years');
+
+textArray = document.querySelector('.output');
+
+//event listeners
+outputInSeconds.addEventListener('click', yearsInSeconds);
+outputInMinutes.addEventListener('click', yearsInMinutes);
+outputInHours.addEventListener('click', yearsInHours);
+outputInMonths.addEventListener('click', yearsInMonths);
+outputInYears.addEventListener('click', years);
+
+//functions
+function yearsInSeconds(){
+    
+};
+
+function yearsInMinutes(){
+
+};
+
+function yearsInHours(){
+
+};
+
+function yearsInMonths(){
+
+};
+
+function years(){
+    
+}
